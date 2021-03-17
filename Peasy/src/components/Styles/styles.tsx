f07e745d57ee8ead134a41813d5ade785304695a
bdanzig <bdanzig@umich.edu>
2021-03-17 19:19:00 -0400

    Finished home page for LoFi Prototype

--- Peasy/src/components/Styles/styles.tsx
+++ Peasy/src/components/Styles/styles.tsx
@@ -4,9 +4,8 @@ export default StyleSheet.create({
   home: {
     justifyContent: 'center',
     alignItems: 'center',
-    marginHorizontal: "auto",
-    paddingHorizontal: 70,
-    maxWidth: 500
+    width: '100%',
+    height: '100%'
   },
   app: {
     justifyContent: 'center',
@@ -34,20 +33,61 @@ export default StyleSheet.create({
   cardH2: {
     fontSize: 17,
     paddingBottom: 20,
-    color: 'lightgrey',
+    color: '#f0f0f0',
+  },
+  recipeH1: {
+    fontSize: 20,
+    color: 'white',
+    backgroundColor: '#3AA43E',
+    alignSelf: 'flex-start',
+    fontWeight: 'bold',
+    padding: 10,
+    borderRadius: 10,
+    overflow: 'hidden',
+    marginBottom: 10
+  },
+  recipeH2: {
+    fontSize: 10,
+    color: 'white',
+    backgroundColor: '#3AA43E',
+    alignSelf: 'flex-start',
+    fontWeight: 'bold',
+    padding: 10,
+    overflow: 'hidden',
+    borderRadius: 10
+  },
+  recipeHL: {
+    fontSize: 10,
+    letterSpacing: 1.3,
+    color: 'white',
+    backgroundColor: '#3AA43E',
+    alignSelf: 'flex-start',
+    marginBottom: 5,
+    padding: 7,
+    borderRadius: 10,
+    overflow: 'hidden'
+  },
+  recipeStat: {
+    fontSize: 10,
+    color: 'white',
+    backgroundColor: '#3AA43E',
+    alignSelf: 'flex-start',
+    padding: 7,
+    marginRight: 4,
+    borderRadius: 10,
+    overflow: 'hidden'
   },
   peasy: {
     fontSize: 50,
-    fontWeight: 'bold',
-    fontStyle: 'italic',
-    paddingBottom: 20,
+    fontFamily: 'Avenir-BlackOblique',
+    paddingBottom: 10,
     color: '#3AA43E',
   },
   logoContainer: {
     paddingVertical: 50,
   },
   card: {
-    paddingVertical: 20
+    paddingVertical: 10
   },
   homeCard1: {
     height: 155,
@@ -58,12 +98,12 @@ export default StyleSheet.create({
     borderRadius: 20,
     shadowColor: "#000",
     shadowOffset: {
-      width: 0,
-      height: 9,
+      width: 1,
+      height: 3,
     },
-    shadowOpacity: 0.48,
-    shadowRadius: 11.95,
-    elevation: 18,
+    shadowOpacity: 0.18,
+    shadowRadius: 1,
+    elevation: 5,
   },
   homeCard2: {
     height: 155,
@@ -74,12 +114,12 @@ export default StyleSheet.create({
     borderRadius: 20,
     shadowColor: "#000",
     shadowOffset: {
-      width: 0,
-      height: 9,
+      width: 1,
+      height: 3,
     },
-    shadowOpacity: 0.48,
-    shadowRadius: 11.95,
-    elevation: 18,
+    shadowOpacity: 0.18,
+    shadowRadius: 1,
+    elevation: 5,
   },
   homeCard3: {
     height: 155,
@@ -90,12 +130,27 @@ export default StyleSheet.create({
     borderRadius: 20,
     shadowColor: "#000",
     shadowOffset: {
-      width: 0,
-      height: 9,
+      width: 1,
+      height: 3,
     },
-    shadowOpacity: 0.48,
-    shadowRadius: 11.95,
-    elevation: 18,
+    shadowOpacity: 0.18,
+    shadowRadius: 1,
+    elevation: 5,
+  },
+  recipeCard1: {
+    height: 155,
+    width: 320,
+    padding: 25,
+    paddingRight: 50,
+    borderRadius: 20,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 1,
+      height: 3,
+    },
+    shadowOpacity: 0.18,
+    shadowRadius: 1,
+    elevation: 5,  
   },
   button: {
     backgroundColor: '#3AA43E',
@@ -106,5 +161,11 @@ export default StyleSheet.create({
   buttonText: {
     color: 'white',
     fontSize: 20,
+  },
+  recipeCardBg: {
+    height: 155,
+    width: 320,
+    opacity: 1,
+    position: 'absolute',
   }
 });
--- Peasy/src/screens/HomeScreen.js
+++ Peasy/src/screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, TouchableOpacity, Image } from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableOpacity, ScrollView, ImageBackground, Image } from 'react-native';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -13,63 +13,131 @@ const HomeScreen = ({ navigation }) => {
     <View style={styles.home}>
       <Peas width={100} height={100}/>
       <Text style={styles.peasy}> peasy </Text>
-      <View>
-        <TouchableOpacity
-          style={styles.homeCard1}
-          onPress={() =>
-            navigation.navigate('Explore')
-          }> 
-          <Text style={styles.cardH1}>Explore new recipes</Text>
-          <Text style={styles.cardH2}>Browse through our expansive database of popular recipes from around the internet.</Text>
-          <View
-            style={[
-              { justifyContent: "absolute",
-                top: -30,
-                left: 255},
-            ]}
-          >
-            <NextIcon height={30} width={30} />
-          </View>
-        </TouchableOpacity>
-      </View>
-      <View style={styles.card}>
-        <TouchableOpacity
-          style={styles.homeCard2}
-          onPress={() =>
-            navigation.navigate('List')
-          }> 
-          <Text style={styles.cardH1}>Build and share your grocery list</Text>
-          <Text style={styles.cardH2}>Choose recipes you want to make and we'll build the list for you.</Text>
-          <View
-            style={[
-              { justifyContent: "absolute",
-                top: -40,
-                left: 255},
-            ]}
-          >
-            <NextIcon height={30} width={30} />
-          </View>
-        </TouchableOpacity>
-      </View>
-      <View>
-        <TouchableOpacity
-          style={styles.homeCard3}
-          onPress={() =>
-            navigation.navigate('Saved')
-          }> 
-          <Text style={styles.cardH1}>Organize and store your favorite recipes</Text>
-          <Text style={styles.cardH2}>No more ripped-out magazine pages or 40-year-old pieces of paper.</Text>
-          <View
-            style={[
-              { justifyContent: "absolute",
-                top: -40,
-                left: 255},
-            ]}
-          >
-            <NextIcon height={30} width={30} />
-          </View>
-        </TouchableOpacity>
-      </View>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={styles.card}>
+          <TouchableOpacity
+            style={styles.homeCard1}
+            onPress={() =>
+              navigation.navigate('Explore')
+            }> 
+            <Text style={styles.cardH1}>Explore new recipes</Text>
+            <Text style={styles.cardH2}>Browse through our expansive database of popular recipes from around the internet.</Text>
+            <View
+              style={[
+                { justifyContent: "absolute",
+                  top: -30,
+                  left: 255},
+              ]}
+            >
+              <NextIcon height={30} width={30} />
+            </View>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.card}>
+          <TouchableOpacity
+            style={styles.recipeCard1}
+            onPress={() =>
+              navigation.navigate('Explore')
+            }> 
+            <ImageBackground 
+              source={require('../assets/salmonpasta.jpeg')}
+              style={styles.recipeCardBg}
+              imageStyle={{ borderRadius: 20 }}
+            />
+            <Text style={styles.recipeHL}> HIGHLIGHTED RECIPE ⭐</Text>
+              <Text style={styles.recipeH1}>Salmon Pea Pasta</Text>
+              <View style={{ flex: 1 }}>
+                  <View style={{ flexDirection: "row", height: 40}}>
+                    <Text style={styles.recipeStat}>Rating: 😋😋😋😋</Text>
+                    <Text style={styles.recipeStat}>Time: ⏰⏰ </Text>
+                    <Text style={styles.recipeStat}>Difficulty: 🔪</Text>
+                  </View>
+              </View>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.card}>
+          <TouchableOpacity
+            style={styles.homeCard2}
+            onPress={() =>
+              navigation.navigate('List')
+            }> 
+            <Text style={styles.cardH1}>Build and share your grocery list</Text>
+            <Text style={styles.cardH2}>Choose recipes you want to make and we'll build the list for you.</Text>
+            <View
+              style={[
+                { justifyContent: "absolute",
+                  top: -40,
+                  left: 255},
+              ]}
+            >
+              <NextIcon height={30} width={30} />
+            </View>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.card}>
+          <TouchableOpacity
+            style={styles.recipeCard1}
+            onPress={() =>
+              navigation.navigate('Explore')
+            }> 
+            <ImageBackground 
+              source={require('../assets/bobby.jpeg')}
+              style={styles.recipeCardBg}
+              imageStyle={{ borderRadius: 20 }}
+            />
+            <Text style={styles.recipeHL}> VIDEO 📺</Text>
+              <Text style={styles.recipeH1}>Easy Risotto with Bobby Flay</Text>
+              <View style={{ flex: 1 }}>
+                  <View style={{ flexDirection: "row", height: 40}}>
+                    <Text style={styles.recipeStat}>Rating: 😋😋😋😋😋</Text>
+                    <Text style={styles.recipeStat}>Time: ⏰⏰⏰ </Text>
+                    <Text style={styles.recipeStat}>Difficulty: 🔪</Text>
+                  </View>
+              </View>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.card}>
+          <TouchableOpacity
+            style={styles.homeCard3}
+            onPress={() =>
+              navigation.navigate('Saved')
+            }> 
+            <Text style={styles.cardH1}>Organize and store your favorite recipes</Text>
+            <Text style={styles.cardH2}>No more ripped-out magazine pages or 40-year-old pieces of paper.</Text>
+            <View
+              style={[
+                { justifyContent: "absolute",
+                  top: -40,
+                  left: 255},
+              ]}
+            >
+              <NextIcon height={30} width={30} />
+            </View>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.card}>
+          <TouchableOpacity
+            style={styles.recipeCard1}
+            onPress={() =>
+              navigation.navigate('Explore')
+            }> 
+            <ImageBackground 
+              source={require('../assets/mealprep.png')}
+              style={styles.recipeCardBg}
+              imageStyle={{ borderRadius: 20 }}
+            />
+            <Text style={styles.recipeHL}> ARTICLE 📚</Text>
+              <Text style={styles.recipeH1}>10 Appliances that Make Meal Prep a Breeze</Text>
+              <View style={{ flex: 1 }}>
+                  <View style={{ flexDirection: "row", height: 40}}>
+                    <Text style={styles.recipeStat}>CONVENIENCE</Text>
+                    <Text style={styles.recipeStat}>MEAL PREP</Text>
+                    <Text style={styles.recipeStat}>PLANNING</Text>
+                  </View>
+              </View>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
     </View>
   );
 }
